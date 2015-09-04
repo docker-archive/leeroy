e814308af7d355586326e83f38e7d0112a2cd470
Jessica Frazelle <acidburn@docker.com>
2015-09-03 22:04:06 -0700

    add back addRepo function
    Signed-off-by: Jessica Frazelle <acidburn@docker.com>

--- github/dco.go
+++ github/dco.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/crosbymichael/octokat"
 )
 
 const (
@@ -93,6 +94,13 @@ func (g GitHub) DcoVerified(pr *PullRequest) (bool, error) {
 	}
 
 	return verified, nil
+}
+
+func getRepo(repo *octokat.Repository) octokat.Repo {
+	return octokat.Repo{
+		Name:     repo.Name,
+		UserName: repo.Owner.Login,
+	}
 }
 
 func labelOs(pr *PullRequest, os string, fileChecker func() bool) bool {
